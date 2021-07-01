322125ddc05cf411345e1b361b72f964df5067ab
k-fine <k-fine@naver.com>
2021-07-01 15:37:28 +0900

    ss

--- src/main/java/spring/north/star/HomeController.java
+++ src/main/java/spring/north/star/HomeController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Handles requests for the application home page.
- */
+     bb*/
 @Controller
 public class HomeController {
 	
